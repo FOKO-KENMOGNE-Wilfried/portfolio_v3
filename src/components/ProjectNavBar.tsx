f52674bdf36628eb930e4773bc1d5615b199dbdf
FOKO KENMOGNE WILFRIED <fokowilfried7@gmail.com>
2025-05-16 19:55:59 +0100

    update start the project layout

--- src/components/ProjectNavBar.tsx
+++ src/components/ProjectNavBar.tsx
@@ -1,6 +1,47 @@
+import type { JSX } from "react";
+import ArrowDownIcon from "./common/icons/ArrowDownIcon";
+import ReactjsIcon from "./common/icons/ReactjsIcon";
+import VuejsIcon from "./common/icons/VuejsIcon";
+import SelectTechno from "./common/SelectTechno";
+
 function ProjectNavBar(){
+
+    const navLinks: {
+        id: number,
+        name: string,
+        icon: JSX.Element,
+        isChecked: boolean
+    }[] = [
+        {
+            id: 1,
+            name: "React",
+            icon: <ReactjsIcon className="w-6 h-6" />,
+            isChecked: false
+        },
+        {
+            id: 2,
+            name: "Vue",
+            icon: <VuejsIcon className="w-6 h-6" />,
+            isChecked: false
+        },
+    ]
+
+    const searchList: string[] = []
+
     return (
-        <div className="w-80 border-r border-secondary-dark"></div>
+        <div className="w-80 border-r border-secondary-dark">
+            <div className="flex items-center w-full h-12 border-b border-secondary-dark pl-8 gap-4">
+                <ArrowDownIcon className="w-6 h-6" />
+                <p>projects</p>
+            </div>
+            <div className="py-4 flex flex-col gap-4">
+                {
+                    navLinks.map((link) => (
+                        <SelectTechno id={link.id} label={link.name} isChecked={link.isChecked} icon={link.icon} handleClick={() => null} />
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/common/SelectTechno.tsx
@@ -0,0 +1,19 @@
+import type { JSX } from "react";
+import CheckIcon from "./icons/CheckIcon";
+
+function SelectTechno({ id, label, icon, isChecked, handleClick } : { id: number, label: string, icon: JSX.Element, isChecked: boolean, handleClick: (label: string) => void }) {
+    return (
+        <div onClick={() => handleClick(label)} className="flex items-center gap-8 pl-8">
+            <div className={`${isChecked ? "bg-secondary-dark text-primary-light" : "text-transparent"} border w-6 h-6 flex justify-center items-center border-secondary-dark rounded-sm transition-all duration-150 ease-in-out`}>
+                <CheckIcon className="w-4 h-4" />
+            </div>
+            {/*  */}
+            <div className="flex gap-2 items-center">
+                { icon }
+                <p>{ label }</p>
+            </div>
+        </div>
+    )
+}
+
+export default SelectTechno;
